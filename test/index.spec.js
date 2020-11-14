3740e64e82aebeb79094f6533132d492fac8ebc2
Hiroki Osame <hiroki.osame@gmail.com>
2020-11-14 18:00:57 -0500

    test: add tests for relative/absolute paths

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,93 @@
-const {System, applyImportMap} = require('systemjs');
-require('systemjs/dist/extras/amd'); // eslint-disable-line import/no-unassigned-import
-require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
-
-const nonExistentPkg = 'pkg-not-on-npm-' + Math.random();
-applyImportMap(System, {
-	imports: {
-		[nonExistentPkg]: 'https://unpkg.com/lodash',
-		underscore: 'https://unpkg.com/lodash',
-	},
+beforeEach(() => {
+	jest.resetModules();
 });
 
 test('should resolve module on UNPKG', async () => {
-	const _ = await System.import('lodash');
-	expect(_).toBeTruthy();
+	const {System} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	await System.import('lodash');
+	expect(spy).toHaveBeenNthCalledWith(2, '//unpkg.com/lodash', undefined);
+
+	spy.mockRestore();
 });
 
 test('should fail when resolving non-existing module', async () => {
+	const {System} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	const nonExistentPkg = 'non-existent-package-for-testing-purposes-' + Math.random();
 	await expect(() =>
-		System.import('non-existent-package-for-testing-purposes-' + Math.random()),
+		System.import(nonExistentPkg),
 	).rejects.toThrow('404 Not Found');
+
+	expect(spy).toHaveBeenNthCalledWith(2, '//unpkg.com/' + nonExistentPkg, undefined);
+
+	spy.mockRestore();
+});
+
+test('should not try to resolve relative paths', async () => {
+	const {System} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	await expect(() =>
+		System.import('./index.js'),
+	).rejects.toThrow('ECONNREFUSED');
+
+	expect(spy).toHaveBeenCalledTimes(1);
+
+	spy.mockRestore();
+});
+
+test('should not try to resolve absolute paths', async () => {
+	const {System} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	await expect(() =>
+		System.import('/index.js'),
+	).rejects.toThrow('ECONNREFUSED');
+
+	expect(spy).toHaveBeenCalledTimes(1);
+
+	spy.mockRestore();
 });
 
 test('should use importmap to resolve non-existing package', async () => {
-	const _ = await System.import(nonExistentPkg);
-	expect(_).toBeTruthy();
+	const {System, applyImportMap} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	const nonExistentPkg = 'pkg-not-on-npm-' + Math.random();
+
+	applyImportMap(System, {
+		imports: {
+			[nonExistentPkg]: 'https://unpkg.com/lodash',
+		},
+	});
+
+	await System.import(nonExistentPkg);
+	expect(spy).toHaveBeenCalledTimes(1);
+
+	spy.mockRestore();
 });
 
 test('should use importmap to resolve underscore to lodash', async () => {
-	const _ = await System.import('underscore');
-	expect(_.default.name).toBe('lodash');
+	const {System, applyImportMap} = require('systemjs');
+	const spy = jest.spyOn(System.constructor.prototype, 'resolve');
+	require('../src/systemjs-unpkg'); // eslint-disable-line import/no-unassigned-import
+
+	applyImportMap(System, {
+		imports: {
+			underscore: 'https://unpkg.com/lodash',
+		},
+	});
+
+	await System.import('underscore');
+	expect(spy).toHaveBeenCalledTimes(1);
+
+	spy.mockRestore();
 });
